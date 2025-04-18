0f5d1d21020dc7b0b9af18e96b55f408d9f4abc2
alex <alexmal24122004@gmail.com>
2025-04-18 16:56:23 +0300

    project v1

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi_pagination import add_pagination
 
 from app.api.routers import main_router
 
-app = FastAPI(title="Форкитех")
+app = FastAPI(title="Trx-Wallets")
 add_pagination(app)
 
 app.include_router(main_router)
